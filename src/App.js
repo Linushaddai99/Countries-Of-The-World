879de7a5cdd56ef15c8a146a5b987336ff054330
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-01-06 00:24:08 +0100

    Add styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import ContriesHomepage from './components/ContriesHomepage';
 import CountriesDetailsPage from './components/ContriesDetailsPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div className="container">
         {/* <ContriesHomepage /> */}
         <Routes>
--- src/components/ContriesDetailsPage.js
+++ src/components/ContriesDetailsPage.js
@@ -3,40 +3,111 @@ import { useLocation } from 'react-router-dom';
 
 const ContriesDetailsPage = () => {
   const location = useLocation();
+  const data = location.state;
 
   return (
-    <div className="details">
-      <div>
-        <img src={location.state.flag} alt="Country flag" />
+    <>
+      <h3 className="details-name">{data.name}</h3>
+      <div className="details">
+        <div className="details-content">
+          <img src={data.flag} alt="Country flag" />
+        </div>
+        <div className="details-content">
+          {
+            data.nativeName
+              ? (
+                <p>
+                  Native Name:
+                  <span className="lighter">{data.nativeName}</span>
+                </p>
+              ) : ''
+          }
+          {
+            data.population
+              ? (
+                <p>
+                  Population:
+                  <span className="lighter">{data.population}</span>
+                </p>
+              ) : ''
+          }
+          {
+            data.region
+              ? (
+                <p>
+                  Region:
+                  <span className="lighter">{data.region}</span>
+                </p>
+              ) : ''
+          }
+          {
+            data.subregion
+              ? (
+                <p>
+                  Sub Region:
+                  <span className="lighter">{data.subregion}</span>
+                </p>
+              ) : ''
+          }
+          {
+            data.capital
+              ? (
+                <p>
+                  Capital:
+                  <span className="lighter">{data.capital}</span>
+                </p>
+              ) : ''
+          }
+        </div>
+        <div className="details-content">
+          {
+            data.currencies
+              ? (
+                <p>
+                  Currency:
+                  <span className="lighter">{data.currencies[0].name}</span>
+                </p>
+              ) : ''
+          }
+          {
+            data.timezones
+              ? (
+                <p>
+                  Timezones:
+                  {data.timezones.map((time) => <span className="details-btn" key={time}>{time}</span>)}
+                </p>
+              ) : ''
+          }
+          {
+            data.topLevelDomain
+              ? (
+                <p>
+                  Top Level Domain:
+                  <span className="lighter">{data.topLevelDomain[0]}</span>
+                </p>
+              ) : ''
+          }
+          {
+            data.languages
+              ? (
+                <p>
+                  Languages:
+                  {data.languages.map((language) => <span className="details-btn" key={language.name}>{language.name}</span>)}
+                </p>
+              ) : ''
+          }
+          {
+            data.borders
+              ? (
+                <p>
+                  Border Countries:
+                  {data.borders.map((border) => <span className="details-btn" key={border}>{border}</span>)}
+                </p>
+              ) : ''
+          }
+        </div>
       </div>
-      <div>
-        <p>{location.state.name}</p>
-        <p>
-          Native Name:
-          {location.state.nativeName}
-        </p>
-        <p>
-          Population:
-          {location.state.population}
-        </p>
-        <p>
-          Continent:
-          {location.state.region}
-        </p>
-        <p>
-          Sub Region:
-          {location.state.subregion}
-        </p>
-        <p>
-          Capital:
-          {location.state.capital}
-        </p>
-        <p>
-          Currency:
-          {location.state.currencies[0].name}
-        </p>
-      </div>
-    </div>
+    </>
   );
 };
 
