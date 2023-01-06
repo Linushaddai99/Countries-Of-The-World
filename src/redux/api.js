3d63aa77a5ad6de8e07c6fd59dc7430b69576d1d
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-01-06 21:21:53 +0100

    Update api.js

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,18 +1,3 @@
-// import { createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// const getCountries = createAsyncThunk('Countries-Of-The-World/getCountries', async () => {
-
-//   try {
-//     const response = await axios.get('https://restcountries.com/v2/all');
-//     return response.data;
-//   } catch (error) {
-//     return error;
-//   }
-// });
-
-// export default getCountries;
-
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const getCountries = createAsyncThunk('Countries-Of-The-World/getCountries', async () => {
